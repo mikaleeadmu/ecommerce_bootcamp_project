fc5c83fdcc725c5e85ae131c861e263148b08f6b
mikaleeadmu <mika.lee@student.ateneo.edu>
2024-12-06 21:06:32 +0800

    Update urls.py

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ecommerce_app.urls')),
+     path('orders/', views.order_list, name='order_list'),
+    path('orders/<int:order_id>/update/', views.update_order_status, name='update_order_status'),
 ]
